c44ce8f4a126d8b77d9e21e3a1d9fbe173298693
Abdou <kirakira1614@gmail.com>
2022-12-29 23:16:53 +0100

    import fileInterceptor

--- src/user/user.controller.ts
+++ src/user/user.controller.ts
@@ -1,7 +1,32 @@
-import { Controller } from '@nestjs/common';
+import { UserInterface } from './interface/user.interface';
+import { CreateUserDto } from './dto/create-user.dto';
+import { Controller, Post, UseInterceptors, Body } from '@nestjs/common';
+import { FileInterceptor } from '@nestjs/platform-express';
 import { UserService } from './user.service';
+import { ApiBadRequestResponse, ApiBody, ApiConsumes, ApiCreatedResponse, ApiInternalServerErrorResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { FileUploadBodyInterceptor } from 'src/common/interceptors/fileUpload.interceptor';
 
+@ApiTags('USERS')
 @Controller('user')
 export class UserController {
   constructor(private readonly userService: UserService) {}
+
+  @ApiCreatedResponse({
+    type: UserInterface,
+    description: 'User Created Successfully'
+  })
+  @ApiBadRequestResponse({
+    description: 'Some User Data Is Invalid'
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Can"t create user ( likely caused by insufficient write permission )'
+  })
+  @ApiBody({ type: CreateUserDto })
+  @ApiOperation({ summary: 'Registration User'})
+  @ApiConsumes('multipart/form-data')
+  @Post('create')
+  @UseInterceptors(FileInterceptor('image'), FileUploadBodyInterceptor)
+  async create(@Body() input: CreateUserDto) {
+    return this.userService.createUser(input)
+  }
 }
