b25a2067d5b3de9abcc364aae94235dfcee00d9c
Abdou <kirakira1614@gmail.com>
2022-12-29 23:16:30 +0100

    import Multer config

--- src/user/user.module.ts
+++ src/user/user.module.ts
@@ -1,8 +1,16 @@
+import { UserSchema } from './schema/user.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { UserService } from './user.service';
 import { UserController } from './user.controller';
+import { MulterModule } from '@nestjs/platform-express';
+import { MulterConfig } from 'src/utils/multer.util';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: 'User', schema: UserSchema }]),
+    MulterModule.register(MulterConfig)
+  ],
   controllers: [UserController],
   providers: [UserService]
 })
