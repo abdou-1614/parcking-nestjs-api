cea60f04118ea587722c45af2f0ca9fbf7e7cbf3
Abdou <kirakira1614@gmail.com>
2022-12-29 23:07:46 +0100

    configuration swagger api

--- src/main.ts
+++ src/main.ts
@@ -1,8 +1,25 @@
 import { NestFactory } from '@nestjs/core';
 import { AppModule } from './app.module';
+import { DocumentBuilder, SwaggerModule } from '@nestjs/swagger';
 
 async function bootstrap() {
   const app = await NestFactory.create(AppModule);
+
+  app.enableCors()
+
+
+  const config = new DocumentBuilder()
+  .setTitle('Parcking Backend')
+  .setDescription('Parcking Management For Backend')
+  .setVersion('0.0.1')
+  .addBearerAuth()
+  .build()
+
+  const document = SwaggerModule.createDocument(app, config)
+  SwaggerModule.setup('doc', app, document, {
+    customSiteTitle: 'Parcking Swagger API'
+  })
+
   await app.listen(3000);
 }
 bootstrap();
