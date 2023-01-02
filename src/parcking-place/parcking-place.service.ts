fc0a954cefed6fa1f9f8518c2dcc456c23917838
Abdou <kirakira1614@gmail.com>
2023-01-02 21:02:21 +0100

    Create Parcking Place Service

--- src/parcking-place/parcking-place.service.ts
+++ src/parcking-place/parcking-place.service.ts
@@ -1,4 +1,19 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { ParckingPlace, ParckingPlaceDocument } from './schema/parcking-place.schema';
+import { Model } from 'mongoose';
+import { CreateParckingPlaceDto } from './dto/create-place.dto';
 
 @Injectable()
-export class ParckingPlaceService {}
+export class ParckingPlaceService {
+    constructor( @InjectModel(ParckingPlace.name) private readonly parckingPlaceModel: Model<ParckingPlaceDocument> ) {}
+
+    async createPlace(input: CreateParckingPlaceDto) {
+        const { name, description } = input
+
+        return this.parckingPlaceModel.create({
+            name,
+            description
+        })
+    }
+}
