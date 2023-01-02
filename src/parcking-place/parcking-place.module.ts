566d9742f3604265ff2510bfc72a25f83a04e05f
Abdou <kirakira1614@gmail.com>
2023-01-02 21:02:53 +0100

    Import Mongoose Module

--- src/parcking-place/parcking-place.module.ts
+++ src/parcking-place/parcking-place.module.ts
@@ -1,8 +1,13 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ParckingPlaceService } from './parcking-place.service';
 import { ParckingPlaceController } from './parcking-place.controller';
+import { ParckingPlace, ParckingPlaceSchema } from './schema/parcking-place.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: ParckingPlace.name, schema: ParckingPlaceSchema }])
+  ],
   controllers: [ParckingPlaceController],
   providers: [ParckingPlaceService]
 })
