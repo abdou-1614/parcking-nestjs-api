44e993d3dfaff94740929cbc9351daed07b9415a
Abdou <kirakira1614@gmail.com>
2023-01-02 21:31:28 +0100

    Query All Parcking Places

--- src/parcking-place/parcking-place.service.ts
+++ src/parcking-place/parcking-place.service.ts
@@ -1,8 +1,11 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { ParckingPlace, ParckingPlaceDocument } from './schema/parcking-place.schema';
 import { Model } from 'mongoose';
 import { CreateParckingPlaceDto } from './dto/create-place.dto';
+import { FilterQueryDto } from 'src/common/dto/filterquery.dto';
+import { FilterQueries } from 'src/utils/filterQueries.util';
+import { ui_query_projection_place } from './parcking-place.projection';
 
 @Injectable()
 export class ParckingPlaceService {
@@ -15,5 +18,23 @@ export class ParckingPlaceService {
             name,
             description
         })
+    }
+
+    async queryAllPlaces(filterDto: FilterQueryDto) {
+        const filterQuery = new FilterQueries(
+            this.parckingPlaceModel,
+            filterDto,
+            ui_query_projection_place
+        )
+
+        filterQuery.filter().limitField().paginate().sort()
+
+        const place = filterQuery.query
+
+        if(!place) {
+            throw new NotFoundException('Parcking Places Not Found')
+        }
+
+        return place
     }
 }
