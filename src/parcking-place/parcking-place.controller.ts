81f077a3b06b39caeab7cf1f04b01891d7772e1d
Abdou <kirakira1614@gmail.com>
2023-01-02 21:03:12 +0100

    Create Parcking Place Controller

--- src/parcking-place/parcking-place.controller.ts
+++ src/parcking-place/parcking-place.controller.ts
@@ -1,7 +1,26 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { ParckingPlaceService } from './parcking-place.service';
+import { CreateParckingPlaceDto } from './dto/create-place.dto';
+import { ApiBadRequestResponse, ApiCreatedResponse, ApiInternalServerErrorResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 
+@ApiTags('PARCKING PLACE')
 @Controller('parcking-place')
 export class ParckingPlaceController {
   constructor(private readonly parckingPlaceService: ParckingPlaceService) {}
+
+
+  @ApiCreatedResponse({
+    description: 'Parcking Place Created Successfully'
+  })
+  @ApiInternalServerErrorResponse({
+    description: 'Can"t create a parcking place ( likely caused by insufficient write permission )'
+  })
+  @ApiBadRequestResponse({
+    description: 'Name Is Required, Please Enter A Name '
+  })
+  @ApiOperation({ summary: 'Create Parcking Place' })
+  @Post()
+  async create(@Body() input: CreateParckingPlaceDto) {
+    return this.parckingPlaceService.createPlace(input)
+  }
 }
