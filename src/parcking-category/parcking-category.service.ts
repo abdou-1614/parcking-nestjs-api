e684f1f0bfd23a098d53137e34681412da734f2e
Abdou <kirakira1614@gmail.com>
2023-01-03 22:40:54 +0100

    Query All Categories

--- src/parcking-category/parcking-category.service.ts
+++ src/parcking-category/parcking-category.service.ts
@@ -1,9 +1,12 @@
 import { HttpException, ServiceUnavailableException } from '@nestjs/common/exceptions';
 import { ParckingCategory, ParckingCategoryDocument } from './schema/parcking-category.schema';
-import { Injectable, HttpStatus } from '@nestjs/common';
+import { Injectable, HttpStatus, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Model } from 'mongoose';
 import { CreateParckingCategoryDto } from './dto/create-parcking-category.dto';
+import { FilterQueryDto } from 'src/common/dto/filterquery.dto';
+import { FilterQueries } from 'src/utils/filterQueries.util';
+import { ui_query_projection_category } from './parcking-category.projection';
 
 @Injectable()
 export class ParckingCategoryService {
@@ -28,5 +31,22 @@ export class ParckingCategoryService {
             }
             throw new ServiceUnavailableException()
         }
+    }
+
+    async queryAllCategories(filterDto: FilterQueryDto) {
+        const filterQuery = new FilterQueries(
+            this.parckingCategoryModel,
+            filterDto,
+            ui_query_projection_category
+        )
+
+        filterQuery.filter().limitField().paginate().sort()
+
+        const category = await filterQuery.query.populate('place', 'name')
+        if(!category) {
+            throw new NotFoundException('Categories Not Found !')
+        }
+
+        return category
     }
 }
