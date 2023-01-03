b58a6d3fa417a15021a1c1698afec4cd37ff8861
Abdou <kirakira1614@gmail.com>
2023-01-03 22:39:49 +0100

    Query All Categories

--- src/parcking-category/parcking-category.controller.ts
+++ src/parcking-category/parcking-category.controller.ts
@@ -1,6 +1,7 @@
-import { Controller, Post, Body } from '@nestjs/common';
+import { FilterQueryDto } from './../common/dto/filterquery.dto';
+import { Controller, Post, Body, Get, Query } from '@nestjs/common';
 import { ParckingCategoryService } from './parcking-category.service';
-import { ApiConflictResponse, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { ApiConflictResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
 import { CreateParckingCategoryDto } from './dto/create-parcking-category.dto';
 
 @ApiTags('PARCKING_CATEGORY')
@@ -18,5 +19,17 @@ export class ParckingCategoryController {
   @Post()
   async create(@Body() input: CreateParckingCategoryDto) {
     return this.parckingCategoryService.createCategory(input)
+  }
+
+  @ApiOkResponse({
+    description: 'Parcking Category Found Successfully'
+  })
+  @ApiNotFoundResponse({
+    description: 'Categories Not Found'
+  })
+  @ApiOperation({ summary: 'Find All Categories With Name Of Parcking Place' })
+  @Get()
+  async findAll(@Query() query: FilterQueryDto) {
+    return this.parckingCategoryService.queryAllCategories(query)
   }
 }
