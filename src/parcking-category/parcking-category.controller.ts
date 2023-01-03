e8b83ed51432055fd1e82a6e852b806473e0d793
Abdou <kirakira1614@gmail.com>
2023-01-03 22:23:58 +0100

    Create Category

--- src/parcking-category/parcking-category.controller.ts
+++ src/parcking-category/parcking-category.controller.ts
@@ -1,7 +1,22 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Post, Body } from '@nestjs/common';
 import { ParckingCategoryService } from './parcking-category.service';
+import { ApiConflictResponse, ApiCreatedResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { CreateParckingCategoryDto } from './dto/create-parcking-category.dto';
 
+@ApiTags('PARCKING_CATEGORY')
 @Controller('parcking-category')
 export class ParckingCategoryController {
   constructor(private readonly parckingCategoryService: ParckingCategoryService) {}
+
+  @ApiCreatedResponse({
+    description: 'Parcking Category Created Successfully'
+  })
+  @ApiConflictResponse({
+    description: 'Category Type Is Already Used'
+  })
+  @ApiOperation({ summary: 'Create Category For Parcking With Places' })
+  @Post()
+  async create(@Body() input: CreateParckingCategoryDto) {
+    return this.parckingCategoryService.createCategory(input)
+  }
 }
