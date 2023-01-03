ebb63755149e368d4f1d52433674279f1a957f2b
Abdou <kirakira1614@gmail.com>
2023-01-03 22:22:55 +0100

    Import Mongoosr Module

--- src/parcking-category/parcking-category.module.ts
+++ src/parcking-category/parcking-category.module.ts
@@ -1,8 +1,13 @@
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { ParckingCategoryService } from './parcking-category.service';
 import { ParckingCategoryController } from './parcking-category.controller';
+import { ParckingCategory, ParckingCategorySchema } from './schema/parcking-category.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: ParckingCategory.name, schema: ParckingCategorySchema }])
+  ],
   controllers: [ParckingCategoryController],
   providers: [ParckingCategoryService]
 })
