7282b00b81ee0c5b5971920e564d35b0c425e724
Abdou <kirakira1614@gmail.com>
2023-01-05 19:43:22 +0100

    import place module

--- src/floor/floor.module.ts
+++ src/floor/floor.module.ts
@@ -1,8 +1,14 @@
+import { ParckingPlace, ParckingPlaceSchema } from './../parcking-place/schema/parcking-place.schema';
+import { MongooseModule } from '@nestjs/mongoose';
 import { Module } from '@nestjs/common';
 import { FloorService } from './floor.service';
 import { FloorController } from './floor.controller';
+import { Floor, FloorSchema } from './schema/floor.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([{ name: Floor.name, schema: FloorSchema }, { name: ParckingPlace.name, schema: ParckingPlaceSchema }])
+  ],
   controllers: [FloorController],
   providers: [FloorService]
 })
