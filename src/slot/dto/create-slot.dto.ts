1100247a503709f659b15e9b5f0e231dfe675726
Abdou <kirakira1614@gmail.com>
2023-01-07 23:13:51 +0100

    first commit

---
+++ src/slot/dto/create-slot.dto.ts
@@ -0,0 +1,62 @@
+import { ApiProperty } from '@nestjs/swagger';
+import { Floor } from './../../floor/schema/floor.schema';
+import { IsNotEmpty, IsString } from 'class-validator';
+import { ParckingPlace } from 'src/parcking-place/schema/parcking-place.schema';
+import { ParckingCategory } from 'src/parcking-category/schema/parcking-category.schema';
+export class CreateSlotDto {
+    @ApiProperty({
+        name: 'name',
+        required: true,
+        description: 'Enter Name OF Slot',
+        example: 'BS10A'
+    })
+    @IsString()
+    @IsNotEmpty()
+    name: string
+
+    @ApiProperty({
+        name: 'remarks',
+        required: false,
+        description: 'Enter Remarks OF Slot',
+        example: 'Nice Slot'
+    })
+    remarks?: string
+
+    @ApiProperty({
+        name: 'identity',
+        required: false,
+        description: 'Enter Identity OF Slot',
+        example: 'ADVCDG545'
+    })
+    identity?: string
+
+    @ApiProperty({
+        name: 'category',
+        required: true,
+        description: 'Enter Category OF Slot',
+        example: '63b33e04e948de2d244f2230'
+    })
+    @IsString()
+    @IsNotEmpty()
+    category: ParckingCategory
+
+    @ApiProperty({
+        name: 'place',
+        required: true,
+        description: 'Enter Place OF Slot',
+        example: '63b33e04e948de2d244f2230'
+    })
+    @IsString()
+    @IsNotEmpty()
+    place: ParckingPlace
+
+    @ApiProperty({
+        name: 'Floor',
+        required: true,
+        description: 'Enter Floor OF Slot',
+        example: '63b71987d4f4219bb227d86b'
+    })
+    @IsString()
+    @IsNotEmpty()
+    floor: Floor
+}
