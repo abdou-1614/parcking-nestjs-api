78eba89d3f933211fc559d0d0781e31413c9bf2a
abdellah hattabi <abdellahhattabi@abdellahs-MacBook-Air.local>
2023-01-12 11:11:26 -0800

    first commit

--- src/slot/slot.service.ts
+++ src/slot/slot.service.ts
@@ -1,4 +1,17 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Model } from 'mongoose';
+import { Slot, SlotDocument } from './schema/slot.schema';
 
 @Injectable()
-export class SlotService {}
+export class SlotService {
+    constructor( @InjectModel(Slot.name) private readonly slotModel: Model<SlotDocument> ){}
+
+    async createSlot(){
+        try{
+            
+        }catch(e){
+
+        }
+    }
+}
