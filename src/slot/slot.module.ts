2479c9cf81aeb1ae59e1f59ce9fd700f3e47a9d5
abdellah hattabi <abdellahhattabi@abdellahs-MacBook-Air.local>
2023-01-14 21:59:39 +0100

    import floor Module

--- src/slot/slot.module.ts
+++ src/slot/slot.module.ts
@@ -3,9 +3,17 @@ import { SlotService } from './slot.service';
 import { SlotController } from './slot.controller';
 import { MongooseModule } from '@nestjs/mongoose';
 import { Slot, SlotSchema } from './schema/slot.schema';
+import { Floor, FloorSchema } from 'src/floor/schema/floor.schema';
+import { ParckingCategory, ParckingCategorySchema } from 'src/parcking-category/schema/parcking-category.schema';
+import { ParckingPlace, ParckingPlaceSchema } from 'src/parcking-place/schema/parcking-place.schema';
 
 @Module({
-  imports: [MongooseModule.forFeature([{ name: Slot.name, schema: SlotSchema }])],
+  imports: [MongooseModule.forFeature([
+    { name: Slot.name, schema: SlotSchema }, 
+    { name: Floor.name, schema: FloorSchema }, 
+    { name: ParckingCategory.name, schema: ParckingCategorySchema }, 
+    { name: ParckingPlace.name, schema: ParckingPlaceSchema }
+  ])],
   controllers: [SlotController],
   providers: [SlotService]
 })
