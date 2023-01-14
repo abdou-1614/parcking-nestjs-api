a67da8dffbbca5837348b6ddd1aebbdf00821d09
abdellah hattabi <abdellahhattabi@abdellahs-MacBook-Air.local>
2023-01-14 22:00:20 +0100

    Create Slot Controller

--- src/slot/slot.controller.ts
+++ src/slot/slot.controller.ts
@@ -1,7 +1,30 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { SlotService } from './slot.service';
+import { CreateSlotDto } from './dto/create-slot.dto';
+import { ApiConflictResponse, ApiCreatedResponse, ApiNotFoundResponse, ApiOperation } from '@nestjs/swagger';
 
 @Controller('slot')
 export class SlotController {
   constructor(private readonly slotService: SlotService) {}
+
+  @ApiCreatedResponse({
+    description: 'Slot Created Successfully !'
+  })
+  @ApiNotFoundResponse({
+    description: 'The Entered Place Not Found'
+  })
+  @ApiNotFoundResponse({
+    description: 'The Entered Category Not Found'
+  })
+  @ApiNotFoundResponse({
+    description: 'The Entered Floor Not Found'
+  })
+  @ApiConflictResponse({
+    description: 'The Name OF Slot Is Already Use'
+  })
+  @ApiOperation({ summary: 'Create Slot' })
+  @Post()
+  async create(@Body() input: CreateSlotDto){
+    return this.slotService.createSlot(input)
+  }
 }
