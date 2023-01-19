de8fc57a87cc96a308dcec09be95ae1cef1e4dde
Abdellah Hattabi <kirakira1614@gmail.com>
2023-01-19 20:30:09 +0100

    first commit

---
+++ src/tariff/tariff.projection.ts
@@ -0,0 +1,10 @@
+export const ui_projection_query_tariff = {
+    name: 1 as any,
+    start_date: 1 as any,
+    end_date: 1 as any,
+    min_amount: 1 as any,
+    hour: 1 as any,
+    place: 1 as any,
+    type: 1 as any,
+    status: 1 as any
+}
