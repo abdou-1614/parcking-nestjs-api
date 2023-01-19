d4cb176746b70a904a03dde7cca9e4e0bb8da9cb
Abdellah Hattabi <kirakira1614@gmail.com>
2023-01-19 20:12:28 +0100

    first commit

--- src/tariff/tariff.module.ts
+++ src/tariff/tariff.module.ts
@@ -1,8 +1,25 @@
 import { Module } from '@nestjs/common';
 import { TariffService } from './tariff.service';
 import { TariffController } from './tariff.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Tariff, TariffSchema } from './schma/tariff.schema';
+import { ParckingCategory, ParckingCategorySchema } from 'src/parcking-category/schema/parcking-category.schema';
+import { ParckingPlace, ParckingPlaceSchema } from 'src/parcking-place/schema/parcking-place.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      {
+       name: Tariff.name, schema: TariffSchema 
+      }, 
+       { 
+        name: ParckingCategory.name, schema: ParckingCategorySchema 
+      },
+      {
+        name: ParckingPlace.name, schema: ParckingPlaceSchema
+      }
+    ])
+  ],
   controllers: [TariffController],
   providers: [TariffService]
 })
