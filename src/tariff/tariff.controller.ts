3bf837807ee470ba053cc380d5da275ef4f98700
Abdellah Hattabi <kirakira1614@gmail.com>
2023-01-19 20:12:33 +0100

    first commit

--- src/tariff/tariff.controller.ts
+++ src/tariff/tariff.controller.ts
@@ -1,7 +1,16 @@
-import { Controller } from '@nestjs/common';
+import { Body, Controller, Post } from '@nestjs/common';
 import { TariffService } from './tariff.service';
+import { CreateTariffDto } from './dto/create-tariff.dto';
+import { ApiTags } from '@nestjs/swagger';
 
+@ApiTags('Tariff')
 @Controller('tariff')
 export class TariffController {
   constructor(private readonly tariffService: TariffService) {}
+
+
+  @Post()
+  async create(@Body() dto: CreateTariffDto){
+    return this.tariffService.createTariff(dto)
+  }
 }
