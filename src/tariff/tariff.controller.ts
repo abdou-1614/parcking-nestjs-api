753d735c4a8aab07f8eafd82c98d66379a874c3c
Abdellah Hattabi <kirakira1614@gmail.com>
2023-01-19 20:30:31 +0100

    query all tariff

--- src/tariff/tariff.controller.ts
+++ src/tariff/tariff.controller.ts
@@ -1,7 +1,8 @@
-import { Body, Controller, Post } from '@nestjs/common';
+import { Body, Controller, Get, Post, Query } from '@nestjs/common';
 import { TariffService } from './tariff.service';
 import { CreateTariffDto } from './dto/create-tariff.dto';
-import { ApiTags } from '@nestjs/swagger';
+import { ApiCreatedResponse, ApiNotFoundResponse, ApiOkResponse, ApiOperation, ApiTags } from '@nestjs/swagger';
+import { FilterQueryDto } from 'src/common/dto/filterquery.dto';
 
 @ApiTags('Tariff')
 @Controller('tariff')
@@ -9,8 +10,30 @@ export class TariffController {
   constructor(private readonly tariffService: TariffService) {}
 
 
+  @ApiCreatedResponse({
+    description: 'Tariff Created Successfully !'
+  })
+  @ApiNotFoundResponse({
+    description: 'The Entered Place Not Found'
+  })
+  @ApiNotFoundResponse({
+    description: 'The Entered Type Not Found'
+  })
+  @ApiOperation({ summary: 'Create Tariff' })
   @Post()
   async create(@Body() dto: CreateTariffDto){
     return this.tariffService.createTariff(dto)
+  }
+
+  @ApiOkResponse({
+    description: 'Tariff Found Successfully'
+  })
+  @ApiNotFoundResponse({
+    description: 'Tariff Not Found !'
+  })
+  @ApiOperation({ summary: 'Find All Tariff' })
+  @Get()
+  async find(@Query() query: FilterQueryDto){
+    return this.tariffService.queryAll(query)
   }
 }
