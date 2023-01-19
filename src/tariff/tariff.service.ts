bd4af7201504f65250126d6915b4a930622ff44d
Abdellah Hattabi <kirakira1614@gmail.com>
2023-01-19 20:30:37 +0100

    query all tariff

--- src/tariff/tariff.service.ts
+++ src/tariff/tariff.service.ts
@@ -1,10 +1,13 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Tariff, TariffDocument } from './schma/tariff.schema';
 import { Model } from 'mongoose';
 import { ParckingPlace, ParckingPlaceDocument } from 'src/parcking-place/schema/parcking-place.schema';
 import { ParckingCategory, ParckingCategoryDocument } from 'src/parcking-category/schema/parcking-category.schema';
 import { CreateTariffDto } from './dto/create-tariff.dto';
+import { FilterQueryDto } from 'src/common/dto/filterquery.dto';
+import { FilterQueries } from 'src/utils/filterQueries.util';
+import { ui_projection_query_tariff } from './tariff.projection';
 
 @Injectable()
 export class TariffService {
@@ -14,8 +17,29 @@ export class TariffService {
         @InjectModel(ParckingCategory.name) private parckingCategoryModel: Model<ParckingCategoryDocument>
     ) {}
 
-    async createTariff(dto: CreateTariffDto){
-        const tariff = await this.tariffModel.create(dto)
+    async createTariff(input: CreateTariffDto){
+        const place = await this.parckingPlaceModel.findById(input.place)
+        if(!place) throw new NotFoundException('The Entered Place Not Found')
+        const type = await this.parckingCategoryModel.findById(input.type)
+        if(!type) throw new NotFoundException('The Entered Type Not Found')
+        const tariff = await this.tariffModel.create(input)
         return tariff
     }
+
+    async queryAll(query: FilterQueryDto){
+        const filterQuery = new FilterQueries(
+            this.tariffModel,
+            query,
+            ui_projection_query_tariff
+        )
+
+        filterQuery.filter().limitField().paginate().sort()
+
+        const tariff = await filterQuery.query.populate('place', 'name').populate('type', 'type')
+
+        if(!tariff) throw new NotFoundException('Tariff Not Found !')
+
+        return tariff
+    }
+
 }
