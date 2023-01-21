45d31de418200e1088879b8b28dd5d83f251131e
Abdellah Hattabi <kirakira1614@gmail.com>
2023-01-21 22:17:29 +0100

    import parking module

--- src/app.module.ts
+++ src/app.module.ts
@@ -4,13 +4,12 @@ import { ParckingPlaceModule } from './parcking-place/parcking-place.module';
 import { ParckingCategoryModule } from './parcking-category/parcking-category.module';
 import { FloorModule } from './floor/floor.module';
 import { TariffModule } from './tariff/tariff.module';
-import { ParckingSetupModule } from './parcking-setup/parcking-setup.module';
-import { ParckingManagementModule } from './parcking-management/parcking-management.module';
 import { ReportsModule } from './reports/reports.module';
 import { SettingModule } from './setting/setting.module';
 import { MongooseModule } from '@nestjs/mongoose';
 import { ConfigModule } from '@nestjs/config';
 import { SlotModule } from './slot/slot.module';
+import { ParckingModule } from './parcking/parcking.module';
 
 @Module({
   imports: [
@@ -21,10 +20,9 @@ import { SlotModule } from './slot/slot.module';
     UserModule, 
     ParckingPlaceModule, 
     ParckingCategoryModule, 
+    ParckingModule,
     FloorModule, 
-    TariffModule, 
-    ParckingSetupModule, 
-    ParckingManagementModule, 
+    TariffModule,
     ReportsModule, 
     SettingModule,
     SlotModule
