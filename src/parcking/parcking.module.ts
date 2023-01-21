e0219ad07ff6f87c998d389209f02eb0dc0a400c
Abdellah Hattabi <kirakira1614@gmail.com>
2023-01-21 22:23:51 +0100

    Import Slot Schema

--- src/parcking/parcking.module.ts
+++ src/parcking/parcking.module.ts
@@ -1,8 +1,21 @@
 import { Module } from '@nestjs/common';
 import { ParckingService } from './parcking.service';
 import { ParckingController } from './parcking.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Parcking, ParckingSchema } from './schema/parcking.schema';
+import { Slot, SlotSchema } from 'src/slot/schema/slot.schema';
+import { ParckingPlace, ParckingPlaceSchema } from 'src/parcking-place/schema/parcking-place.schema';
+import { ParckingCategory, ParckingCategorySchema } from 'src/parcking-category/schema/parcking-category.schema';
 
 @Module({
+  imports: [
+    MongooseModule.forFeature([
+      { name: Parcking.name, schema: ParckingSchema },
+      { name: Slot.name, schema: SlotSchema },
+      { name: ParckingPlace.name, schema: ParckingPlaceSchema },
+      { name: ParckingCategory.name, schema: ParckingCategorySchema }
+    ])
+  ],
   controllers: [ParckingController],
   providers: [ParckingService]
 })
