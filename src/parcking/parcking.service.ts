458f3a6fbe22390dddf1b437cb4b190d4e93db6c
Abdellah Hattabi <kirakira1614@gmail.com>
2023-01-25 19:57:21 +0100

    first commit

--- src/parcking/parcking.service.ts
+++ src/parcking/parcking.service.ts
@@ -1,4 +1,47 @@
-import { Injectable } from '@nestjs/common';
+import { HttpException, HttpStatus, Injectable, NotFoundException, ServiceUnavailableException } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Parcking, ParckingDocument } from './schema/parcking.schema';
+import { Model } from 'mongoose';
+import { Slot, SlotDocument } from 'src/slot/schema/slot.schema';
+import { ParckingPlace, ParckingPlaceDocument } from 'src/parcking-place/schema/parcking-place.schema';
+import { ParckingCategory, ParckingCategoryDocument } from 'src/parcking-category/schema/parcking-category.schema';
+import { CreateParckingDto } from './dto/create-parcking.dto';
 
 @Injectable()
-export class ParckingService {}
+export class ParckingService {
+    constructor( 
+        @InjectModel(Parcking.name) private parckingModel: Model<ParckingDocument>,
+        @InjectModel(Slot.name) private slotgModel: Model<SlotDocument>,
+        @InjectModel(ParckingPlace.name) private parckingPlaceModel: Model<ParckingPlaceDocument>,
+        @InjectModel(ParckingCategory.name) private ParckingCategoryModel: Model<ParckingCategoryDocument>, 
+        ){}
+
+        async createParcking(input: CreateParckingDto){
+            const place = await this.parckingPlaceModel.findById(input.place)
+            if(!place) throw new NotFoundException('Place Not Found')
+
+            const type = await this.ParckingCategoryModel.findById(input.type)
+            if(!type) throw new NotFoundException('Type Not Found')
+
+            const slot = await this.slotgModel.findById(input.slot)
+            if(!slot) throw new NotFoundException('Slot Not Found')
+            
+            try{
+                const parking = await this.parckingModel.create(input)
+                return parking
+            }catch(e){
+                if(e.code === 11000){
+                    const dublicatedKey = Object.values(e.keyValue)[0]
+                    throw new HttpException({
+                        statusCode: HttpStatus.CONFLICT,
+                        message: `${dublicatedKey} Already Exist`,
+                        dublicatedKey
+                    }, HttpStatus.CONFLICT)
+                }
+
+                console.log(e)
+
+                throw new ServiceUnavailableException(e)
+            }
+        }
+}
