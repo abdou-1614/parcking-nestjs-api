a9a6a3db885eba21fd4d4bbacc4c233ea5c59582
Abdellah Hattabi <kirakira1614@gmail.com>
2023-01-28 00:57:21 +0100

    first commit

--- src/parcking/parking.projection.ts
+++ src/parcking/parking.projection.ts
@@ -2,5 +2,6 @@ export const ui_projection_query_parking = {
     qrCode: 1 as any,
     vehicle_Number: 1 as any,
     in_time: 1 as any,
-    out_time: 1 as any
+    out_time: 1 as any,
+    payable_amount: 1 as any
 }
