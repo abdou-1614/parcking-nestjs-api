801aa5ace503ca2c3dd3d94273c178b47bac83ac
Abdellah Hattabi <kirakira1614@gmail.com>
2023-02-04 22:02:18 +0100

    first commit

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -1,7 +1,15 @@
-import { Controller } from '@nestjs/common';
+import { Controller, Get, Query } from '@nestjs/common';
 import { ReportsService } from './reports.service';
+import { ApiTags } from '@nestjs/swagger';
+import { SummaryReportDto } from './dto/summary-reports.dto';
 
+@ApiTags('REPORTS')
 @Controller('reports')
 export class ReportsController {
   constructor(private readonly reportsService: ReportsService) {}
+
+  @Get('slot-report')
+  async getReports(@Query() query: SummaryReportDto){
+    return this.reportsService.getReport(query)
+  }
 }
