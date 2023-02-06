93b689a453989e80229e58295ed837794817fff1
Abdellah Hattabi <kirakira1614@gmail.com>
2023-02-06 12:52:25 +0100

    get slot report

--- src/reports/reports.service.ts
+++ src/reports/reports.service.ts
@@ -22,90 +22,93 @@ export class ReportsService {
     ){}
 
     async getReport(inputQuery: SummaryReportDto){
+      let match = {}
+      if (inputQuery.floor) {
         const floorName = await this.floorModel.findById(inputQuery.floor)
-        const slotReport = await this.parckingPlaceModel.aggregate([
-            {
-                $lookup: {
-                  from: "floors",
-                  localField: "_id",
-                  foreignField: "place",
-                  as: "floors"
-                }
-              },
-              {
-                $unwind: "$floors"
-              },
-              {
-                $lookup: {
-                  from: "slots",
-                  localField: "floors._id",
-                  foreignField: "floor",
-                  as: "slots"
-                }
-              },
-              {
-                $unwind: "$slots"
-              },
-              {
-                $lookup: {
-                  from: "parkingcategories",
-                  localField: "slots.category",
-                  foreignField: "_id",
-                  as: "category"
-                }
-              },
-              {
-                $group: {
-                  _id: { place: "$name", floor: "$floors.name", category: "$category.type" },
-                  totalSlots: { $sum: 1 },
-                  bookedSlots: {
-                    $sum: {
-                      $cond: [
-                        { $eq: ["$slots.status", "INACTIVE"] },
-                        1,
-                        0
-                      ]
-                    }
-                  },
-                  availableSlots: {
-                    $sum: {
-                      $cond: [
-                        { $eq: ["$slots.status", "ACTIVE"] },
-                        1,
-                        0
-                      ]
-                    }
-                  },
-                  availableSlotName: {
-                    $push: {
-                      $cond: [
-                        { $eq: ["$slots.status", "ACTIVE"] },
-                        "$slots.name",
-                        null
-                      ]
-                    }
-                  }
-                }
-              },
-              {
-                $project: {
-                  _id: 0,
-                  place: "$_id.place",
-                  floor: "$_id.floor",
-                  category: "$_id.category",
-                  totalSlots: 1,
-                  bookedSlots: 1,
-                  availableSlots: 1,
-                  availableSlotName: {
-                    $filter: {
-                      input: "$availableSlotName",
-                      as: "slotName",
-                      cond: { $ne: ["$$slotName", null] }
-                    }
-                  }
-                }
+        match = { "floors": floorName }
+      }
+      if (inputQuery.place) {
+        const placeName = await this.parckingPlaceModel.findById(inputQuery.place)
+        match = {...match, "name": placeName.name }
+      }
+    
+      const slotReport = await this.parckingPlaceModel.aggregate([
+        {
+          $lookup: {
+            from: "floors",
+            localField: "_id",
+            foreignField: "place",
+            as: "floors"
+          }
+        },
+        {
+          $unwind: "$floors"
+        },
+        {
+          $lookup: {
+            from: "slots",
+            localField: "floors._id",
+            foreignField: "floor",
+            as: "slots"
+          }
+        },
+        {
+          $unwind: "$slots"
+        },
+        {
+          $match: match
+        },
+        {
+          $group: {
+            _id: { place: "$name", floor: "$floors.name", category: "$category.type" },
+            totalSlots: { $sum: 1 },
+            bookedSlots: {
+              $sum: {
+                $cond: [
+                  { $eq: ["$slots.status", "INACTIVE"] },
+                  1,
+                  0
+                ]
               }
-        ])
-        return slotReport
+            },
+            availableSlots: {
+              $sum: {
+                $cond: [
+                  { $eq: ["$slots.status", "ACTIVE"] },
+                  1,
+                  0
+                ]
+              }
+            },
+            availableSlotName: {
+              $push: {
+                $cond: [
+                  { $eq: ["$slots.status", "ACTIVE"] },
+                  "$slots.name",
+                  null
+                ]
+              }
+            },
+          }
+        },
+        {
+          $project: {
+            _id: 0,
+            place: "$_id.place",
+            floor: "$_id.floor",
+            totalSlots: 1,
+            bookedSlots: 1,
+            availableSlots: 1,
+            availableSlotName: {
+              $filter: {
+                input: "$availableSlotName",
+                as: "slotName",
+                cond: { $ne: ["$$slotName", null] }
+              }
+            }
+          }
+        }
+      ]);
+      return slotReport;
     }
 }
