67fdd18f74b9cfdd9515b7dc773fe3a0cccf3473
Abdellah Hattabi <kirakira1614@gmail.com>
2023-02-06 12:51:40 +0100

    first commit

--- src/parcking-category/parcking-category.module.ts
+++ src/parcking-category/parcking-category.module.ts
@@ -4,10 +4,17 @@ import { Module } from '@nestjs/common';
 import { ParckingCategoryService } from './parcking-category.service';
 import { ParckingCategoryController } from './parcking-category.controller';
 import { ParckingCategory, ParckingCategorySchema } from './schema/parcking-category.schema';
+import { Floor, FloorSchema } from 'src/floor/schema/floor.schema';
+import { Slot, SlotSchema } from 'src/slot/schema/slot.schema';
 
 @Module({
   imports: [
-    MongooseModule.forFeature([{ name: ParckingCategory.name, schema: ParckingCategorySchema }, { name: ParckingPlace.name, schema: ParckingPlaceSchema }])
+    MongooseModule.forFeature([{ 
+      name: ParckingCategory.name, schema: ParckingCategorySchema }, 
+      { name: ParckingPlace.name, schema: ParckingPlaceSchema },
+      { name: Slot.name, schema: SlotSchema },
+      { name: Floor.name, schema: FloorSchema },
+    ])
   ],
   controllers: [ParckingCategoryController],
   providers: [ParckingCategoryService]
