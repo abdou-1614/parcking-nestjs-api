6dc3edd3b722b59f6c50a44f60cbaf8e4ca0b10f
Abdellah Hattabi <kirakira1614@gmail.com>
2023-02-09 01:57:27 +0100

    get details report

--- src/reports/reports.controller.ts
+++ src/reports/reports.controller.ts
@@ -2,6 +2,7 @@ import { Controller, Get, Query } from '@nestjs/common';
 import { ReportsService } from './reports.service';
 import { ApiTags } from '@nestjs/swagger';
 import { SummaryReportDto } from './dto/summary-reports.dto';
+import { DetailsReportsDto } from './dto/details-reports.dto';
 
 @ApiTags('REPORTS')
 @Controller('reports')
@@ -15,5 +16,9 @@ export class ReportsController {
   @Get('summary-report')
   async SummaryReport(@Query() query: SummaryReportDto){
     return this.reportsService.getSummaryReport(query)
+  }
+  @Get('details-report')
+  async DetailsReport(@Query() query: DetailsReportsDto){
+    return this.reportsService.detailsReport(query)
   }
 }
