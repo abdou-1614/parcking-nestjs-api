eb8a20c646eb3c3137873ecc87dcf20cfc5b751f
Abdellah Hattabi <kirakira1614@gmail.com>
2023-02-10 01:03:33 +0100

    first commit

--- src/setting/setting.service.ts
+++ src/setting/setting.service.ts
@@ -1,4 +1,18 @@
 import { Injectable } from '@nestjs/common';
+import { InjectModel } from '@nestjs/mongoose';
+import { Setting, SettingDocument } from './schema/setting.schema';
+import { Model } from 'mongoose';
+import { CreateSettingDto } from './dto/create-setting.dto';
 
 @Injectable()
-export class SettingService {}
+export class SettingService {
+    constructor( @InjectModel(Setting.name) private settingModel: Model<SettingDocument> ){}
+
+    async createSetting(input: CreateSettingDto){
+        const setting = await this.settingModel.create({
+            name: input.name,
+            image: input.image.filename
+        })
+        return setting
+    }
+}
