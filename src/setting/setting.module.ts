01cfba2cabc7274b2d44928e9828119e9ec806cb
Abdellah Hattabi <kirakira1614@gmail.com>
2023-02-10 01:03:26 +0100

    first commit

--- src/setting/setting.module.ts
+++ src/setting/setting.module.ts
@@ -1,8 +1,18 @@
 import { Module } from '@nestjs/common';
 import { SettingService } from './setting.service';
 import { SettingController } from './setting.controller';
+import { MongooseModule } from '@nestjs/mongoose';
+import { Setting, SettingSchema } from './schema/setting.schema';
+import { MulterModule } from '@nestjs/platform-express';
+import { MulterConfig } from 'src/utils/multer.util';
 
 @Module({
+  imports: [
+    MulterModule.register(MulterConfig),
+    MongooseModule.forFeature([
+      { name: Setting.name, schema: SettingSchema }
+    ])
+  ],
   controllers: [SettingController],
   providers: [SettingService]
 })
