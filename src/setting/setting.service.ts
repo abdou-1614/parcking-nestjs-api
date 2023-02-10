350dbe345c7564f50a8a05c076dc091768f447e9
Abdellah Hattabi <kirakira1614@gmail.com>
2023-02-10 01:31:49 +0100

    first commit

--- src/setting/setting.service.ts
+++ src/setting/setting.service.ts
@@ -1,8 +1,10 @@
-import { Injectable } from '@nestjs/common';
+import { Injectable, NotFoundException } from '@nestjs/common';
 import { InjectModel } from '@nestjs/mongoose';
 import { Setting, SettingDocument } from './schema/setting.schema';
 import { Model } from 'mongoose';
 import { CreateSettingDto } from './dto/create-setting.dto';
+import { FilterQueryDto } from 'src/common/dto/filterquery.dto';
+import { FilterQueries } from 'src/utils/filterQueries.util';
 
 @Injectable()
 export class SettingService {
@@ -13,6 +15,21 @@ export class SettingService {
             name: input.name,
             image: input.image.filename
         })
+        return setting
+    }
+
+    async findAll(inputQuery: FilterQueryDto){
+        const filterQuery = new FilterQueries(
+            this.settingModel,
+            inputQuery
+        )
+        await filterQuery.filter().limitField().paginate().sort()
+
+        const setting = await filterQuery.query
+
+        if(setting.length === 0){
+            throw new NotFoundException('No Info Found')
+        }
         return setting
     }
 }
