43164147415b129e27082516868b2100ab9dc1dc
Abdellah Hattabi <kirakira1614@gmail.com>
2023-02-10 01:31:58 +0100

    first commit

--- src/setting/schema/setting.schema.ts
+++ src/setting/schema/setting.schema.ts
@@ -7,8 +7,12 @@ export type SettingDocument = Setting & mongoose.Document
 export class Setting {
     @Prop({ type: String })
     name?: string
+
     @Prop({ type: String })
     image?: string
+
+    @Prop({ type: Boolean, default: false })
+    isActive: boolean
 }
 
 export const SettingSchema = SchemaFactory.createForClass(Setting)
