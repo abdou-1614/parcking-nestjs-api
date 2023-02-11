feecd3c20b8030b395e84aa26851155dc7d8bfc5
Abdellah Hattabi <kirakira1614@gmail.com>
2023-02-11 01:35:39 +0100

    first commit

---
+++ src/common/guards/role.guard.ts
@@ -0,0 +1,27 @@
+import { CanActivate, ExecutionContext, Injectable } from "@nestjs/common";
+import { Reflector } from "@nestjs/core";
+import { Observable } from "rxjs";
+import { IS_ADMIN_KEY } from "../decorators/is-admin.decorator";
+import { ACCOUNT_ROLE } from "src/constants/account.constant";
+
+@Injectable()
+export class RoleGuard implements CanActivate {
+    constructor( private reflector: Reflector ){}
+
+    canActivate(context: ExecutionContext): boolean | Promise<boolean> | Observable<boolean> {
+        const isAdmin = this.reflector.getAllAndOverride(IS_ADMIN_KEY, [
+            context.getClass(),
+            context.getHandler()
+        ])
+
+        const request = context.switchToHttp().getRequest()
+
+        const { role } = request.user
+
+        if(isAdmin && role === ACCOUNT_ROLE.ADMIN){
+            return true
+        }
+
+        return false
+    }
+}
