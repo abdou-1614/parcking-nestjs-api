6a84b6995d068b019129461a73cf4ce2b1f34924
Abdellah Hattabi <kirakira1614@gmail.com>
2023-03-31 01:45:22 +0100

    first commit

--- src/auth/access-jwt.strategy.ts
+++ src/auth/access-jwt.strategy.ts
@@ -5,22 +5,30 @@ import { accessJwtConfig } from "src/utils/access-Jwt-Config";
 import { AccessTokenPayload } from "./types/access-token-payload";
 import { AccessTokenContent } from "./types/access-token-content";
 import { Injectable } from "@nestjs/common";
+import { InjectModel } from "@nestjs/mongoose";
+import { Model } from "mongoose";
+import { User, UserDocument } from "src/user/schema/user.schema";
 
 @Injectable()
 export class AccessTokenJwtStrategy extends PassportStrategy(Strategy, 'access-token') {
-    constructor(){
+    constructor(
+        public configService: ConfigService,
+        @InjectModel(User.name) private userModel: Model<UserDocument>
+    ){
         super({
             jwtFromRequest: ExtractJwt.fromAuthHeaderAsBearerToken(),
             ignoreExpiration: false,
-            secretOrKey: 'SECRET@JWT@1416'
+            secretOrKey: configService.get('JWT_SECRET')
         })
     }
 
 
     async validate(payload: AccessTokenPayload): Promise<AccessTokenContent>{
+        const user = await this.userModel.findById(payload.sub)
         return {
             id: payload.sub,
-            userRole: payload.userRole
+            userRole: payload.userRole,
+            ...user
         }
     }
 }
