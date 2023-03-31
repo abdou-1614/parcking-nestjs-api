07f65922657923e8ef3edb509904cb42591274b7
Abdellah Hattabi <kirakira1614@gmail.com>
2023-03-31 01:45:32 +0100

    first commit

--- src/auth/auth.controller.ts
+++ src/auth/auth.controller.ts
@@ -1,11 +1,17 @@
-import { Body, Controller, Get, Post, Req } from "@nestjs/common";
+import { Body, Controller, Get, Post, Req, UseInterceptors } from "@nestjs/common";
 import { AuthService } from "./auth.service";
-import { ApiBadRequestResponse, ApiBearerAuth, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
+import { ApiBadRequestResponse, ApiBearerAuth, ApiBody, ApiConsumes, ApiOkResponse, ApiOperation, ApiTags } from "@nestjs/swagger";
 import { LoginDto } from "./dto/login.dto";
 import { Public } from "./public.decorator";
 import { LoginResponseDto } from "./dto/login-response.dto";
 import { UpdatePasswordDto } from "./dto/update-password.dto";
 import { Request } from "express";
+import { RefreshTokenDto } from "./dto/refreshToken.dto";
+import { LogoutDto } from "./dto/logout.dto";
+import { IsAdmin } from "src/common/decorators/is-admin.decorator";
+import { UpdateProfileDto } from "./dto/update-profile.dto";
+import { FileInterceptor } from "@nestjs/platform-express";
+import { FileUploadBodyInterceptor } from "src/common/interceptors/fileUpload.interceptor";
 
 @ApiTags('AUTH')
 @Controller('auth')
@@ -20,9 +26,50 @@ export class AuthController{
     })
     @ApiOperation({ description: 'User Login' })
     @Public()
-    @Get()
-    async Login(@Body() input: LoginDto): Promise<LoginResponseDto>{
-        return this.authService.login(input)
+    @Post()
+    async Login(@Body() input: LoginDto, @Req() request: Request): Promise<LoginResponseDto>{
+        const browserInfo = `${request.ip} ${request.headers['user-agent']} ${request.headers['accept-language']}`.replace(/ undefined/g, '')
+
+        return this.authService.login(input, browserInfo)
+    }
+    @ApiOkResponse({ type: LoginResponseDto })
+    @ApiOperation({ summary: 'RefereshToken' })
+    @Public()
+    @Post('/refreshToken')
+    async refreshToken(@Req() request: Request, @Body() { refreshToken }: RefreshTokenDto): Promise<LoginResponseDto> {
+      const browserInfo = `${request.ip} ${request.headers['user-agent']} ${request.headers['accept-language']}`.replace(/ undefined/g, '')
+  
+      return this.authService.refreshToken(refreshToken, browserInfo)
+    }
+    @ApiOperation({ summary: 'Log Out User' })
+    @ApiBearerAuth()
+    @Post('/logout')
+    async logout(@Body() { refreshToken }: LogoutDto) {
+      return this.authService.logout(refreshToken)
+    }
+  
+    @ApiOperation({ summary: 'Logs out user of all sessions' })
+    @ApiBearerAuth()
+    @Post('/logoutAll')
+    async logouAll(@Req() request: Request) {
+      const { id } = request.user as { id: string };;
+      return this.authService.logoutAll(id)
+    }
+
+    @IsAdmin()
+    @Get('/my')
+    @ApiBearerAuth()
+    async findCurrentLogged(@Req() request: Request){
+        const { id } = request.user as { id: string };
+        return this.authService.findMe(id)
+    }
+  
+    @ApiOperation({ summary: 'Returns all user active tokens' })
+    @ApiBearerAuth()
+    @Get('/tokens')
+    async findAllTokens(@Req() request: Request){
+              const { id } = request.user as { id: string };;
+      return this.authService.findAllTokens(id)
     }
 
     @ApiBadRequestResponse({
@@ -37,5 +84,22 @@ export class AuthController{
     async changePassword(@Req() request: Request, @Body() input: UpdatePasswordDto): Promise<LoginResponseDto> {
         const { id } = request.user as { id: string }
         return this.authService.changePassword(id, input)
+    }
+
+    @ApiBadRequestResponse({
+        description: 'the entered password is invalid'
+    })
+    @ApiOkResponse({
+        type: LoginResponseDto
+    })
+    @ApiBody({ type: UpdateProfileDto })
+    @ApiConsumes('multipart/form-data')
+    @ApiBearerAuth()
+    @ApiOperation({ description: 'User Update Current Profile Information' })
+    @Post('/profile')
+    @UseInterceptors(FileInterceptor('image'), FileUploadBodyInterceptor)
+    async updateProfile(@Req() request: Request, @Body() input: UpdateProfileDto): Promise<LoginResponseDto>{
+        const { id } = request.user as { id: string }
+        return this.authService.updateProfile(id, input)
     }
 }
