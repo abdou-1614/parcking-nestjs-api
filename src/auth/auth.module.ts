baa983366bac89eb39858672deaf9168313d4e67
Abdellah Hattabi <kirakira1614@gmail.com>
2023-03-31 01:45:39 +0100

    first commit

--- src/auth/auth.module.ts
+++ src/auth/auth.module.ts
@@ -6,14 +6,19 @@ import { JwtModule } from "@nestjs/jwt";
 import { AccessTokenJwtStrategy } from "./access-jwt.strategy";
 import { MongooseModule } from "@nestjs/mongoose";
 import { User, UserSchema } from "src/user/schema/user.schema";
+import { RefreshToken, RefreshTokenSchema } from "./schema/tokens.schema";
+import { MulterModule } from "@nestjs/platform-express";
+import { MulterConfig } from "src/utils/multer.util";
 
 @Module({
     imports: [
-        MongooseModule.forFeature([{
-            name: User.name, schema: UserSchema
-        }]),
+        MongooseModule.forFeature([
+            {name: User.name, schema: UserSchema,},
+            {name: RefreshToken.name, schema: RefreshTokenSchema}
+    ]),
         PassportModule,
-        JwtModule.register({})
+        JwtModule.register({}),
+        MulterModule.register(MulterConfig)
     ],
     providers: [AuthService, AccessTokenJwtStrategy],
     controllers: [AuthController]
